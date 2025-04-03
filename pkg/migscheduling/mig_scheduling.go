f6e77dbc18d3a6bbd43d4d352c511bec519604eb
Yuhang Wei <weiyuhang2011@126.com>
2025-04-03 21:21:50 +0000

    refactore: rename to matrix-scheduler
    Signed-off-by: Yuhang Wei <weiyuhang2011@126.com>

--- pkg/migscheduling/mig_scheduling.go
+++ pkg/migscheduling/mig_scheduling.go
@@ -2,14 +2,10 @@ package migscheduling
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
-	"strings"
 
 	v1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
@@ -35,46 +31,46 @@ func (pl *MigScheduling) Name() string {
 	return Name
 }
 
-// func (pl *MigScheduling) Filter(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodeInfo *framework.NodeInfo) *framework.Status {
-// 	log.Printf("filter pod: %v, node: %v\n", pod.Name, nodeInfo)
-// 	log.Println(state)
-
-// 	// 排除没有cpu=true标签的节点
-// 	if nodeInfo.Node().Labels["cpu"] != "true" {
-// 		return framework.NewStatus(framework.Unschedulable, "Node: "+nodeInfo.Node().Name)
-// 	}
-// 	return framework.NewStatus(framework.Success, "Node: "+nodeInfo.Node().Name)
-// }
-
 func (pl *MigScheduling) Filter(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodeInfo *framework.NodeInfo) *framework.Status {
-	klog.Infof("Start filtering node: %s, pod: %s", nodeInfo.Node().Name, pod.Name)
-	migrators, err := listMigrators(ctx, pl.dynclient, "")
-	if err != nil {
-		klog.ErrorS(err, "Failed to list migrators")
-		return framework.NewStatus(framework.Error, "")
+	if node, ok := pod.Annotations["origin.node.name"]; ok {
+		klog.Infof("Pod %s is migrated from node %s", pod.Name, node)
+		if node == nodeInfo.Node().Name {
+			klog.Infof("Pod %s is migrated to node %s", pod.Name, nodeInfo.Node().Name)
+			return framework.NewStatus(framework.Unschedulable, "Pod is migrated to this node")
+		}
 	}
-	if len(migrators.Items) == 0 {
-		klog.Infof("No migrators found")
-		return framework.NewStatus(framework.Success, "No migrators found")
-	} else if len(migrators.Items) > 1 {
-		klog.Infof("Too many migrators, dont know which nodes to be scheduled")
-		return framework.NewStatus(framework.Error, "Too many migrators, dont know which nodes to be scheduled")
-	}
-	migrator := migrators.Items[0]
-	// get pod
-	podName := migrator.Spec.PodName
-	podNamespace := migrator.Spec.PodNameSpace
-	klog.Infof("Migrator %s PodName: %s, PodNamespace: %s", migrator.Name, podName, podNamespace)
-	deployName := strings.Split(podName, "-")[0]
-	newPodDeployName := strings.Split(pod.Name, "-")[0]
-	if deployName == newPodDeployName && migrator.Spec.Destination != nodeInfo.Node().Name {
-		klog.Infof("Migrate Destination: %s, Current Node: %s, return error", migrator.Spec.Destination, nodeInfo.Node().Name)
-		return framework.NewStatus(framework.Unschedulable, "Failed to schedule pod to not destination node: "+nodeInfo.Node().Name)
-	}
-
-	klog.Infof("Migrate Destination: %s, Current Node: %s, return success", migrator.Spec.Destination, nodeInfo.Node().Name)
 	return framework.NewStatus(framework.Success, "Node: "+nodeInfo.Node().Name)
 }
+
+// func (pl *MigScheduling) Filter(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodeInfo *framework.NodeInfo) *framework.Status {
+// 	klog.Infof("Start filtering node: %s, pod: %s", nodeInfo.Node().Name, pod.Name)
+// 	migrators, err := listMigrators(ctx, pl.dynclient, "")
+// 	if err != nil {
+// 		klog.ErrorS(err, "Failed to list migrators")
+// 		return framework.NewStatus(framework.Error, "")
+// 	}
+// 	if len(migrators.Items) == 0 {
+// 		klog.Infof("No migrators found")
+// 		return framework.NewStatus(framework.Success, "No migrators found")
+// 	} else if len(migrators.Items) > 1 {
+// 		klog.Infof("Too many migrators, dont know which nodes to be scheduled")
+// 		return framework.NewStatus(framework.Error, "Too many migrators, dont know which nodes to be scheduled")
+// 	}
+// 	migrator := migrators.Items[0]
+// 	// get pod
+// 	podName := migrator.Spec.PodName
+// 	podNamespace := migrator.Spec.PodNameSpace
+// 	klog.Infof("Migrator %s PodName: %s, PodNamespace: %s", migrator.Name, podName, podNamespace)
+// 	deployName := strings.Split(podName, "-")[0]
+// 	newPodDeployName := strings.Split(pod.Name, "-")[0]
+// 	if deployName == newPodDeployName && migrator.Spec.Destination != nodeInfo.Node().Name {
+// 		klog.Infof("Migrate Destination: %s, Current Node: %s, return error", migrator.Spec.Destination, nodeInfo.Node().Name)
+// 		return framework.NewStatus(framework.Unschedulable, "Failed to schedule pod to not destination node: "+nodeInfo.Node().Name)
+// 	}
+
+// 	klog.Infof("Migrate Destination: %s, Current Node: %s, return success", migrator.Spec.Destination, nodeInfo.Node().Name)
+// 	return framework.NewStatus(framework.Success, "Node: "+nodeInfo.Node().Name)
+// }
 
 func New(obj runtime.Object, handle framework.Handle) (framework.Plugin, error) {
 	klog.Infof("Initializing MigScheduling plugin")
@@ -115,44 +111,44 @@ func New(obj runtime.Object, handle framework.Handle) (framework.Plugin, error) 
 	}, nil
 }
 
-func getMigrator(ctx context.Context, client dynamic.Interface, name, namespace string) (*Migrator, error) {
-	migrator, err := client.Resource(getGVR("migrate.openeuler.org", "v1alpha1", "migrators")).Namespace(namespace).Get(ctx, name, metav1.GetOptions{})
-	if err != nil {
-		klog.ErrorS(err, "Failed to get migrator")
-		return nil, err
-	}
-	var mig Migrator
-	if err := runtime.DefaultUnstructuredConverter.FromUnstructured(migrator.UnstructuredContent(), &mig); err != nil {
-		klog.ErrorS(err, "Failed to convert unstructured to struct")
-		return nil, err
-	}
+// func getMigrator(ctx context.Context, client dynamic.Interface, name, namespace string) (*Migrator, error) {
+// 	migrator, err := client.Resource(getGVR("migrate.openeuler.org", "v1alpha1", "migrators")).Namespace(namespace).Get(ctx, name, metav1.GetOptions{})
+// 	if err != nil {
+// 		klog.ErrorS(err, "Failed to get migrator")
+// 		return nil, err
+// 	}
+// 	var mig Migrator
+// 	if err := runtime.DefaultUnstructuredConverter.FromUnstructured(migrator.UnstructuredContent(), &mig); err != nil {
+// 		klog.ErrorS(err, "Failed to convert unstructured to struct")
+// 		return nil, err
+// 	}
 
-	return &mig, nil
-}
+// 	return &mig, nil
+// }
 
-func listMigrators(ctx context.Context, client dynamic.Interface, namespace string) (*MigratorList, error) {
-	if namespace == "" {
-		namespace = "default"
-	}
-	migrators, err := client.Resource(getGVR("migrate.openeuler.org", "v1alpha1", "migrators")).Namespace(namespace).List(ctx, metav1.ListOptions{})
-	if err != nil {
-		klog.ErrorS(err, "Failed to list migrators")
-		return nil, err
-	}
-	data, err := migrators.MarshalJSON()
-	if err != nil {
-		klog.ErrorS(err, "Failed to marshal migrators")
-		return nil, err
-	}
-	var migList MigratorList
-	if err := json.Unmarshal(data, &migList); err != nil {
-		klog.ErrorS(err, "Failed to unmarshal migrators")
-		return nil, err
-	}
-	return &migList, nil
-}
+// func listMigrators(ctx context.Context, client dynamic.Interface, namespace string) (*MigratorList, error) {
+// 	if namespace == "" {
+// 		namespace = "default"
+// 	}
+// 	migrators, err := client.Resource(getGVR("migrate.openeuler.org", "v1alpha1", "migrators")).Namespace(namespace).List(ctx, metav1.ListOptions{})
+// 	if err != nil {
+// 		klog.ErrorS(err, "Failed to list migrators")
+// 		return nil, err
+// 	}
+// 	data, err := migrators.MarshalJSON()
+// 	if err != nil {
+// 		klog.ErrorS(err, "Failed to marshal migrators")
+// 		return nil, err
+// 	}
+// 	var migList MigratorList
+// 	if err := json.Unmarshal(data, &migList); err != nil {
+// 		klog.ErrorS(err, "Failed to unmarshal migrators")
+// 		return nil, err
+// 	}
+// 	return &migList, nil
+// }
 
-// getGVR :- gets GroupVersionResource for dynamic client
-func getGVR(group, version, resource string) schema.GroupVersionResource {
-	return schema.GroupVersionResource{Group: group, Version: version, Resource: resource}
-}
+// // getGVR :- gets GroupVersionResource for dynamic client
+// func getGVR(group, version, resource string) schema.GroupVersionResource {
+// 	return schema.GroupVersionResource{Group: group, Version: version, Resource: resource}
+// }
